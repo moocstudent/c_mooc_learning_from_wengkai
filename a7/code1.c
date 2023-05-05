85f1420ee9b73164c1be04c5e804df38cf14063e
moocstudent <deadzq@qq.com>
2023-05-05 13:09:38 +0800

    note2

---
+++ a7/code1.c
@@ -0,0 +1,41 @@
+//
+// Created by tanghuijuan on 2023/5/5.
+//
+#include <stdio.h>
+#include <string.h>
+
+void compareStr(char str1,char str2,int leftLenth) {
+    int result;
+    result = strncmp(str1,str2,4);
+
+    if(result > 0) {
+        printf("str1的第一个不匹配字符的 ASCII 值大于str2");
+    }else if(result < 0) {
+        printf("str1的第一个不匹配字符的 ASCII 值小于str2");
+    }else {
+        printf("所比较给定位数的字符串相等");
+    }
+}
+
+int main() {
+    char str1[20];
+    char str2[20];
+
+
+    strcpy(str1,"hello");
+
+    strcpy(str2,"helLO WORLD");
+
+//    result = strncmp(str1,str2,3);
+    result = strncmp(str1,str2,4);
+
+    if(result > 0) {
+        printf("str1的第一个不匹配字符的 ASCII 值大于str2");
+    }else if(result < 0) {
+        printf("str1的第一个不匹配字符的 ASCII 值小于str2");
+    }else {
+        printf("所比较给定位数的字符串相等");
+    }
+
+    return 0;
+}
