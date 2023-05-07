56b116486ff8a7131be3160d34d3ff46f75d548b
moocstudent <deadzq@qq.com>
2023-05-07 17:17:16 +0800

    今天在刷youtube，看得小lin。

--- a7/code1.c
+++ a7/code1.c
@@ -4,22 +4,12 @@
 #include <stdio.h>
 #include <string.h>
 
-void compareStr(char str1,char str2,int leftLenth) {
-    int result;
-    result = strncmp(str1,str2,4);
 
-    if(result > 0) {
-        printf("str1的第一个不匹配字符的 ASCII 值大于str2");
-    }else if(result < 0) {
-        printf("str1的第一个不匹配字符的 ASCII 值小于str2");
-    }else {
-        printf("所比较给定位数的字符串相等");
-    }
-}
 
 int main() {
     char str1[20];
     char str2[20];
+    int result = 0;
 
 
     strcpy(str1,"hello");
